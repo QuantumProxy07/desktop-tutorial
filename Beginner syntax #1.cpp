1aa88a920ab62313909cec6d11195e79194922c4
QuantumProxy <bitupantalukdar13@gmail.com>
2026-09-01 18:39:23 +0530

    Added basic c++ syntax example with explanation
    This section shows the basic starter syntax which is needed to run any code of c++ , it also holds the explanation of each part , feel free to correct me

---
+++ Beginner syntax #1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+// the #include <iostream> part tells the computer to recognize (specifically iostream) cmds like cout , cin , endl 
+
+using namespace std;
+
+// the using namespace std part tells the computer (specifically std) to recognize cmds from library such as cout , vecotor , int ,etc 
+
+int main()
+
+// the int main() part is where the main executable codes starts from 
+{
+
+    cout << "Dark Soul" << endl;
+
+// cout is like the print cmd which tells to print a variable value or a value itself 
+    
+return 0;
+
+    // return 0 is where the executable code part ends , if the code is correct after running the codes the computer will give output as code = 0 which means all corrct 
+
+}
